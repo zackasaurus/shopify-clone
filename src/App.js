2e28690ed910ad3212419048972c6aba0cc2cbcf
SUWAPA PUTTAPOONTRAKUL <Namnam@suwapas-air.attlocal.net>
2020-05-06 20:24:11 -0400

    add homepage skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component.jsx';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
