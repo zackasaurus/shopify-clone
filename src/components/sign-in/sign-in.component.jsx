2189c92cc3c1b9829fc9b2d75b52f120a0894861
zach <zachbogard@gmail.com>
2020-05-09 17:55:03 -0400

    intergate firebase, sign in pages

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -40,11 +40,13 @@ export class SignIn extends Component {
             label="Password"
             required
           />
-          <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {''}
-            Sign in with Google{''}
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign in</CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {''}
+              Sign in with Google{''}
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
