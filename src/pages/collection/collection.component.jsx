0307198644b59478d1f6d0c0d8184922ad6b298b
zach <zachbogard@gmail.com>
2020-05-17 12:38:38 -0400

    Finished preview, overview with corresponding selectors

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import './collection.styles.scss';
-import { selectCollection } from '../../redux/shop/shop.selector';
+
 import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <div className="category">
+    <div className="collection-page">
       <h2 className="title">{title}</h2>
       <div className="items">
-        {items.map((item) => {
-          return <CollectionItem key={item.id} item={item} />;
-        })}
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
       </div>
     </div>
   );
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    collection: selectCollection(ownProps.match.params.collectionId)(state),
-  };
-};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
 
 export default connect(mapStateToProps)(CollectionPage);
